837fec3f037d3cad5dcd4d4e22022bf99a10ffa7
Aditya <anandaditya2528@gmail.com>
2024-02-10 23:47:19 +0530

    todo-list completed

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -1,15 +1,31 @@
 let inputbt = document.querySelector('#inputbt');
 let list = document.querySelector('#list'); 
+let addbt = document.querySelector('#addbt');
 
+
+addbt.addEventListener("click" , function(){
+  if(inputbt.value.trim() != ''){
+    addItem(inputbt.value.trim());
+    inputbt.value = '' ; 
+  }
+}) ; 
 inputbt.addEventListener("keyup" , function(event){
   if(event.key == "Enter"){
-    addItem(this.value)
-    this.value = "" 
+    addbt.click(); 
   }
-})
+});
 
 let addItem = (inputbt) => {
   let listItem = document.createElement("li"); 
-  listItem.innerHTML= `${inputbt}<i><i>`; 
+  listItem.innerHTML= `${inputbt}<button class="remove-button">Remove</button>`; 
+
+  listItem.addEventListener("click" , function(event){
+    if (event.target.classList.contains('remove-button')) {
+      this.remove(); 
+    } else {
+      this.classList.toggle('done'); 
+    }
+  })
+
   list.appendChild(listItem); 
 }
